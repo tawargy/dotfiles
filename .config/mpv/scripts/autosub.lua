5275211803016e01dddba16bac3b8179fffeb8df
Mohamed Tawargy <tawargi.m@gmail.com>
2022-02-18 02:48:29 +0200

    modify

--- .config/mpv/scripts/autosub.lua
+++ .config/mpv/scripts/autosub.lua
@@ -255,6 +255,6 @@ function log(string, secs)
 end
 
 
-mp.add_key_binding('b', 'download_subs', download_subs)
-mp.add_key_binding('n', 'download_subs2', download_subs2)
+mp.add_key_binding('a', 'download_subs', download_subs)
+mp.add_key_binding('e', 'download_subs2', download_subs2)
 mp.register_event('file-loaded', control_downloads)
